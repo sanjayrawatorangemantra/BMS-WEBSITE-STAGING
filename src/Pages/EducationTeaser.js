12c98aabc4abfb65aa374ce0257d92a7bc0112c0
deepak <93deep93@gmail.com>
2020-12-23 16:34:09 +0530

    dynamic teaser.

--- src/Pages/EducationTeaser.js
+++ src/Pages/EducationTeaser.js
@@ -45,7 +45,9 @@ class EducationTeaser extends React.Component {
       rating_per:0,
       is_block_user : false,
       is_show_teaser_model : false,
-      teaserContent : ''
+      teaserContent : '',
+      teaserContentIndex :0,
+      allTeaserContent : []
     };
   }
 
@@ -87,17 +89,19 @@ class EducationTeaser extends React.Component {
       })
     ).then(res => {
           if(res.data.data)
-          this.setState({ teaserContent : res.data.data[0].fld_content })
+          this.setState({ teaserContent : res.data.data[0].fld_content, allTeaserContent :  res.data.data , teaserContentIndex : 0})
       });
     });
   }
 
-  
+  gotoNextTeaser=()=>{
+    this.setState({ teaserContentIndex: this.state.teaserContentIndex+1})
+  }
  
 
 
   render() {
-    const { rating, rating_per, is_block_user , teaserContent} = this.state;
+    const { rating, rating_per, is_block_user , teaserContent, allTeaserContent, teaserContentIndex} = this.state;
 
       var log = localStorage.getItem(
         "CustomerLoginDetails"
@@ -156,31 +160,48 @@ class EducationTeaser extends React.Component {
               </div>
                 <div className="container" style={{background:"none"}}>
                     <div className="row mt-2">
-                        <div className="col-lg-8 order-lg-first ">
+                        <div className={ teaserContentIndex === allTeaserContent.length-1 ? "col-lg-8 order-lg-first ": "col-lg-12 order-lg-first"}>
                         
                           <div className="dashboard-content">
                             <h3>Welcome to BMS Education </h3>
-                                <div dangerouslySetInnerHTML= {{__html: teaserContent }}></div> 
+                                <div dangerouslySetInnerHTML= {{__html: allTeaserContent.length>0 ? allTeaserContent[teaserContentIndex].fld_content :'' }}></div> 
                                 <div class="navlinks" style={{ justifyContent:'center'}}>
+                                   {teaserContentIndex === allTeaserContent.length-1 && teaserContentIndex === 0 ? 
+                                   
                                   <div class="navlinkbutton next">
-                                    <button class="activelink" style={{ float:'right'}} onClick={ ()=>{ this.props.history.push('/education')  }} >Enter <span><img src="/assets/images/next.png"/></span> </button>
+                                    <button class="activelink" style={{ float:'right'}} onClick={ ()=>{
+                                      this.props.history.push('/education') 
+                                        }} >Enter <span><img src="/assets/images/next.png"/></span> </button>
                                   </div>
+                                  : teaserContentIndex === 0?
+                                  <div class="navlinkbutton next">
+                                    <button class="activelink" style={{ float:'right'}} onClick={ ()=>{
+                                      this.setState({teaserContentIndex : teaserContentIndex+1 })
+                                        }} >Next <span><img src="/assets/images/next.png"/></span> </button>
+                                  </div>:
+                                  <>
+                                    <div class="prevquestion">
+                                      <button class="prev" onClick={ ()=>{ 
+                                        this.setState({teaserContentIndex : teaserContentIndex-1 }); 
+                                        }}><span><img src="/assets/images/arrow.png"/></span> <span>Previous</span>
+                                      </button>
+                                    </div>
+                                    
+                                    <div class="navlinkbutton next">
+                                      <button class="activelink" style={{ float:'right'}} onClick={ ()=>{
+                                        {teaserContentIndex === allTeaserContent.length-1 ?
+                                          this.props.history.push('/education') :
+                                          this.setState({teaserContentIndex : teaserContentIndex+1 }) }
+                                          }} > {teaserContentIndex === allTeaserContent.length-1 ? 'Enter':'Next'} <span><img src="/assets/images/next.png"/></span> 
+                                      </button>
+                                    </div>
+                                  </>
+                                  }
                                 </div>
-                               {/* <div
-                                style={{
-                                  width: "100%",
-                                  height: "100",
-                                  display: "flex",
-                                  justifyContent: "center",
-                                  alignItems: "center"
-                                }}
-                              >
-                                <Loader type="ThreeDots" color="#00BFFF" height="50" width="50" />
-                              </div> */}
                             </div>
                         
                        </div>
-                        <div className="col-lg-4">
+                        <div className="col-lg-4" style={{ display :  teaserContentIndex === allTeaserContent.length-1 ?'block':'none' }}>
                             <div className="course-side-bar">
                                 { login != null && login != "" ? 
                                     <div className="login-box">
