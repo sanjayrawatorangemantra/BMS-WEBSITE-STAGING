09e65aeb25da537b5c3a452884c74e690b80237c
deepak <93deep93@gmail.com>
2021-01-06 17:11:48 +0530

    update sitemap.

---
+++ build/precache-manifest.166be707e971aa06222b95af39a6eef8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49eaa3e614fc3c1729c6494bb9a40587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "513268471e66af35c3d3",
+    "url": "/static/css/2.5a958046.chunk.css"
+  },
+  {
+    "revision": "fc96c8fb09450dd1435b",
+    "url": "/static/css/main.5db8509d.chunk.css"
+  },
+  {
+    "revision": "513268471e66af35c3d3",
+    "url": "/static/js/2.fd13420d.chunk.js"
+  },
+  {
+    "revision": "5c57d0be02213fc74be6bbe7c98ed2bb",
+    "url": "/static/js/2.fd13420d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc96c8fb09450dd1435b",
+    "url": "/static/js/main.518dd2db.chunk.js"
+  },
+  {
+    "revision": "d3ef2486b38192aa22a3",
+    "url": "/static/js/runtime-main.8c3fa017.js"
+  },
+  {
+    "revision": "69c43aabcbe618e2952dc9a22b29254e",
+    "url": "/static/media/course.69c43aab.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f1dd0f4ca8476b0e8847fcfa5703689.js"
+  "/precache-manifest.166be707e971aa06222b95af39a6eef8.js"
 );
 
 self.addEventListener('message', (event) => {
